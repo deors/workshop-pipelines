1bc9e009fdbe3da5fe80b490bddd64489d12f89c
Jorge Hidalgo <jorge.hidalgo@gmail.com>
2022-06-01 12:30:08 +0200

    rename app test file/class

--- src/test/java/deors/demos/java/pipeline/JavaPipelineApplicationTest.java
+++ src/test/java/deors/demos/java/pipeline/JavaPipelineApplicationTest.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
-public class JavaPipelineApplicationTests {
+public class JavaPipelineApplicationTest {
 
 	@Test
 	public void contextLoads() {
